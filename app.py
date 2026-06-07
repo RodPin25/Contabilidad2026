db9a39acfd85a18337377838b3480741c651b158
JonathanUS-706 <jonathanson706@gmail.com>
2026-06-06 19:18:45 -0600

    Sincronizacion del app.py de forma general con config & CORS(app)

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, redirect, url_for, jsonify
-from config import Config                     
+from flask_cors import CORS                         # <-- 1. Importas la librería
+from config import Config                       
 from database.connection import init_db
 from routes.diario import router as diario_bp  
 
 app = Flask(__name__)
-app.config.from_object(Config)                
+CORS(app)                                      
+app.config.from_object(Config)                  
 init_db(app)
 
-# Registro de tu Blueprint contable en la arquitectura global
 app.register_blueprint(diario_bp)  
 
 @app.route('/')
